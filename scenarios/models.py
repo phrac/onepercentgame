a683da17aa3e298ff7fec3d4fb3f795ea271408e
Derek Musselmann <derek@disflux.com>
2015-08-29 08:00:32 -0500

    Update models to reflect the seven sins. Also better define the Scenarios models

--- scenarios/models.py
+++ scenarios/models.py
@@ -3,9 +3,24 @@ from django.db import models
 
 class Scenario(models.Model):
     question = models.TextField()
-    karma_value = models.NumberField()
-    outcomes = models.ManyToManyField(Outcome)
-
+    level = models.IntegerField(max_length=3)
+    
+class ScenarioChoice(models.Model):
+    scenario = models.ForeignKey(Scenario)
 
 class Outcome(models.Model):
+    scenario = models.ForeignKey(ScenarioChoice)
+    karma_value = models.DecimalField(max_digits=6, decimal_places=2)
+    wrath_value = models.IntegerField(default=0, null=True)
+    greed_value = models.IntegerField(default=0, null=True)
+    sloth_value = models.IntegerField(default=0, null=True)
+    pride_value = models.IntegerField(default=0, null=True)
+    lust_value = models.IntegerField(default=0, null=True)
+    envy_value = models.IntegerField(default=0, null=True)
+    gluttony_value = models.IntegerField(default=0, null=True)
+
+    outcome_text = models.TextField()
+    
+
+
     
