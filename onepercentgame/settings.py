683e3e1755d0f89b328596f1968e090ea4dfbcd4
Derek Musselmann <derek@disflux.com>
2015-08-29 09:31:12 -0500

    Define local settings file

--- onepercentgame/settings.py
+++ onepercentgame/settings.py
@@ -95,8 +95,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+from settings_local import *
